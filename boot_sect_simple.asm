b3f108ed755292f495e2a7107402d547fb912550
evanblasy <evanblasy@gmail.com>
2020-11-24 15:04:25 -0500

    part 02

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,6 +1,13 @@
-; Infinite Loop
-loop:
-	jmp loop
+mov ah, 0x0e
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
 
 ; 510 zeros
 times 510-($-$$) db 0
