2edc04796f8563d5fa8321f5d300c29a471b757f
evanblasy <evanblasy@gmail.com>
2020-11-24 15:20:03 -0500

    part 03.1

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,13 +1,32 @@
 mov ah, 0x0e
-mov al, 'H'
+
+mov al, "1"
 int 0x10
-mov al, 'e'
+mov al, the_secret
 int 0x10
-mov al, 'l'
+
+mov al, "2"
 int 0x10
+mov al, [the_secret]
 int 0x10
-mov al, 'o'
+
+mov al, "3"
 int 0x10
+mov bx, the_secret
+mov bx, 0x7C00
+mov al, [bx]
+int 0x10
+
+mov al, "4"
+int 0x10
+mov al, [0x7c2c]
+int 0x10
+
+jmp $
+
+the_secret:
+	db "X"
+
 
 ; 510 zeros
 times 510-($-$$) db 0
